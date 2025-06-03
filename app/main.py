999562a6d3d33dc93054d3c9660f289123fc7935
vitaliy-pvs <vitaliypryhodko@yandex.com>
2025-06-03 07:37:34 +0300

    first commit

---
+++ app/main.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI
+from utils import json_to_dict_list
+import os
+from typing import Optional
+
+path_to_json = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'students.json')
+
+app = FastAPI()
+
+
+@app.get("/")
+def home_page():
+    return {"message": "Привет, Хабр!"}
+
+
+@app.get("/students/{course}")
+def get_all_students_course(course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018):
+    students = json_to_dict_list(path_to_json)
+    filtered_students = []
+    for student in students:
+        if student["course"] == course:
+            filtered_students.append(student)
+
+    if major:
+        filtered_students = [student for student in filtered_students if student['major'].lower() == major.lower()]
+
+    if enrollment_year:
+        filtered_students = [student for student in filtered_students if student['enrollment_year'] == enrollment_year]
+
+    return filtered_students
+
+
+@app.get("/students")
+def get_all_students(course: Optional[int] = None):
+    students = json_to_dict_list(path_to_json)
+    if course is None:
+        return students
+    else:
+        return_list = []
+        for student in students:
+            if student["course"] == course:
+                return_list.append(student)
+        return return_list

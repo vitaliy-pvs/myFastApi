c4a2b9ca4041effa0a8d9325b86ff86e78086897
vitaliy-pvs <vitaliypryhodko@yandex.com>
2025-06-10 20:54:16 +0300

    POST method complete

--- app/main.py
+++ app/main.py
@@ -1,12 +1,19 @@
-from fastapi import FastAPI
-from utils import json_to_dict_list
+from fastapi import FastAPI, Depends
+from utils import *
 import os
-from typing import Optional
+from typing import Optional, List
 from app.student import Student as SStudent
 
 path_to_json = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'students.json')
 
 app = FastAPI()
+
+
+class RBStudent:
+    def __init__(self, course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018):
+        self.course: int = course
+        self.major: Optional[str] = major
+        self.enrollment_year: Optional[int] = enrollment_year
 
 
 @app.get("/")
@@ -21,19 +28,22 @@ def get_student_from_param_id(student_id: int) -> SStudent:
         if student["student_id"] == student_id:
             return student
 
+
 @app.get("/students/{course}")
-def get_all_students_course(course: int, major: Optional[str] = None, enrollment_year: Optional[int] = 2018):
+def get_all_students_course(request_body: RBStudent = Depends()) -> List[SStudent]:
     students = json_to_dict_list(path_to_json)
     filtered_students = []
     for student in students:
-        if student["course"] == course:
+        if student["course"] == request_body.course:
             filtered_students.append(student)
 
-    if major:
-        filtered_students = [student for student in filtered_students if student['major'].lower() == major.lower()]
+    if request_body.major:
+        filtered_students = [student for student in filtered_students if
+                             student['major'].lower() == request_body.major.lower()]
 
-    if enrollment_year:
-        filtered_students = [student for student in filtered_students if student['enrollment_year'] == enrollment_year]
+    if request_body.enrollment_year:
+        filtered_students = [student for student in filtered_students if
+                             student['enrollment_year'] == request_body.enrollment_year]
 
     return filtered_students
 
@@ -49,3 +59,13 @@ def get_all_students(course: Optional[int] = None):
             if student["course"] == course:
                 return_list.append(student)
         return return_list
+
+
+@app.post("/add_student")
+def add_student_handler(student: SStudent):
+    student_dict = student.dict()
+    check = add_student(student_dict)
+    if check:
+        return {"message": "Студент успешно добавлен!"}
+    else:
+        return {"message": "Ошибка при добавлении студента"}
--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import json
+from json_db_lite import JSONDatabase
 
 
 def dict_list_to_json(dict_list, filename):
@@ -34,3 +35,31 @@ def json_to_dict_list(filename):
     except (TypeError, ValueError, IOError) as e:
         print(f"Ошибка при чтении JSON из файла или преобразовании в список словарей: {e}")
         return None
+
+
+# инициализация объекта
+small_db = JSONDatabase(file_path='students.json')
+
+
+# получаем все записи
+def json_to_dict_list():
+    return small_db.get_all_records()
+
+
+# добавляем студента
+def add_student(student: dict):
+    student['date_of_birth'] = student['date_of_birth'].strftime('%Y-%m-%d')
+    small_db.add_records(student)
+    return True
+
+
+# обновляем данные по студенту
+def upd_student(upd_filter: dict, new_data: dict):
+    small_db.update_record_by_key(upd_filter, new_data)
+    return True
+
+
+# удаляем студента
+def dell_student(key: str, value: str):
+    small_db.delete_record_by_key(key, value)
+    return True
